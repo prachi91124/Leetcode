c8303ea137065cad1ea9b7f9e2d629118abfa2f7
Prachi Anand <prachianand321@gmail.com>
2025-01-21 15:45:58 +0530

    'https://leetcode.com/problems/string-matching-in-an-array/submissions/1500863625' String Matching in an array solved in cpp

---
+++ strmatchinginaanarr.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    vector<string> stringMatching(vector<string> &words)
+    {
+        // resultant array of strings
+        vector<string> ans;
+
+        for (int i = 0; i < words.size(); i++)
+        {
+            for (int j = 0; j < words.size(); j++)
+            {
+                if (i != j && words[j].find(words[i]) != string::npos)
+                {
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
